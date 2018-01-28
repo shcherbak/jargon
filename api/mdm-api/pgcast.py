29d00e643467999a910e85c49f8c899d367b09c6
shcherbak <ju.shcherbak@gmail.com>
2018-01-28 04:14:42 +0200

    pgcast refactoring

--- api/mdm-api/pgcast.py
+++ api/mdm-api/pgcast.py
@@ -10,72 +10,168 @@ import psycopg2.extras
 import re
 
 
-def _get_pg_nspname_oid(conn, nspname):
-    _sql = 'SELECT oid FROM pg_namespace WHERE nspname = %s'
-    _connection = conn
-    try:
-        _curs = _connection.cursor()
-        _curs.execute(_sql, (nspname,))
-        _oid = _curs.fetchone()[0]
-        _curs.close()
-        return _oid
-    except (Exception, psycopg2.DatabaseError) as error:
-        print(error)
+def pg_typ_caster(connection, nspname, typname, mapclass):
+    def _get_pg_nspname_oid():
+        _sql = 'SELECT oid FROM pg_namespace WHERE nspname = %s'
+        try:
+            _curs = connection.cursor()
+            _curs.execute(_sql, (nspname,))
+            _oid = _curs.fetchone()[0]
+            _curs.close()
+            return _oid
+        except (Exception, psycopg2.DatabaseError) as error:
+            print(error)
+
+    def _get_pg_typname_oid():
+        _sql = 'SELECT oid FROM pg_type WHERE typname = %s AND typnamespace = %s;'
+        try:
+            _nspoid = _get_pg_nspname_oid()
+            _curs = connection.cursor()
+            _curs.execute(_sql, (typname, _nspoid,))
+            _oid = _curs.fetchone()[0]
+            _curs.close()
+            return _oid
+        except (Exception, psycopg2.DatabaseError) as error:
+            print(error)
+
+    def _get_pg_typarray_oid():
+        _sql = 'SELECT typarray FROM pg_type WHERE typname = %s AND typnamespace = %s'
+        try:
+            _nspoid = _get_pg_nspname_oid()
+            _curs = connection.cursor()
+            _curs.execute(_sql, (typname, _nspoid,))
+            _oid = _curs.fetchone()[0]
+            _curs.close()
+            return _oid
+        except (Exception, psycopg2.DatabaseError) as error:
+            print(error)
+
+    oid1 = _get_pg_typname_oid()
+    oid2 = _get_pg_typarray_oid()
+
+    pg_udf_type = _ext.new_type((oid1,), typname.upper(), mapclass)
+    pg_udf_type_array = _ext.new_array_type((oid2,), "{0}_ARRAY".format(typname.upper()), pg_udf_type)
+
+    _ext.register_type(pg_udf_type, connection)
+    _ext.register_type(pg_udf_type_array, connection)
+
+    return pg_udf_type
 
 
-def _get_pg_typname_oid(conn, nspname, typname):
-    _sql = 'SELECT oid FROM pg_type WHERE typname = %s AND typnamespace = %s;'
-    _connection = conn
-    try:
-        _nspoid = _get_pg_nspname_oid(_connection, nspname)
-        _curs = _connection.cursor()
-        _curs.execute(_sql, (typname, _nspoid,))
-        _oid = _curs.fetchone()[0]
-        _curs.close()
-        return _oid
-    except (Exception, psycopg2.DatabaseError) as error:
-        print(error)
+class PgUserTypeMaping(object):
+    pg_schm_name = ''
+    pg_type_name = ''
+    pg_field_list = []
 
+    def __conform__(self, proto):
+        if proto == _ext.ISQLQuote:
+            return self
 
-def _get_pg_typarray_oid(conn, nspname, typname):
-    _sql = 'SELECT typarray FROM pg_type WHERE typname = %s AND typnamespace = %s'
-    _connection = conn
-    try:
-        _nspoid = _get_pg_nspname_oid(_connection, nspname)
-        _curs = _connection.cursor()
-        _curs.execute(_sql, (typname, _nspoid,))
-        _oid = _curs.fetchone()[0]
-        _curs.close()
-        return _oid
-    except (Exception, psycopg2.DatabaseError) as error:
-        print(error)
+    def date_from_py(self, pydate):
+        pass
 
+    def date_from_pg(self, sqldate):
+        pass
 
+    def decimal_from_py(self, pydecimal):
+        pass
 
-def date_from_py(pydate):
-    pass
+    def decimal_from_pg(self, sqlnumeric):
+        pass
 
-def date_from_pg(sqldate):
-    pass
-
-def decimal_from_py(pydecimal):
-    pass
-
-def decimal_from_pg(sqlnumeric):
-    pass
-
-def _adapt(o):
-    if o is None:
-        return 'NULL'
-    else:
-        if isinstance(o, str):
-            return "'{0}'".format(o)
+    @staticmethod
+    def _adapt(o):
+        if o is None:
+            return 'NULL'
         else:
-            # return _ext.adapt(obj=o, alternate=None, protocol=None)
-            return _ext.adapt(o)
+            # return _ext.adapt(o)
+            if isinstance(o, str):
+                return "'{0}'".format(o)
+            else:
+                # return _ext.adapt(obj=o, alternate=None, protocol=None)
+                return _ext.adapt(obj=o, alternate=None, protocol=None)
+
+    _re_tokenize = re.compile(r"""
+      \(? ([,)])                        # an empty token, representing NULL
+    | \(? " ((?: [^"] | "")*) " [,)]    # or a quoted string
+    | \(? ([^",)]+) [,)]                # or an unquoted string
+        """, re.VERBOSE)
+
+    _re_undouble = re.compile(r'(["\\])\1')
+
+    def from_tuple(self, t):
+        return self
+
+    def to_tuple(self):
+        return ()
+
+    def from_dict(self, d):
+        return self
+
+    def to_dict(self):
+        return {}
+
+    def __repr__(self):
+        return self.repr_helper(self.pg_field_list).format(t=self.pg_type_name, d=self.to_dict())
+
+    @staticmethod
+    def repr_helper(field_list):
+        result = '{t}=('
+        idx = 1
+        length = len(field_list)
+        for item in field_list:
+            if idx < length:
+                result = result + "{0}={1}{0}{2} ".format(item, '{d[', ']},')
+            else:
+                result = result + "{0}={1}{0}{2} ".format(item, '{d[', ']})')
+            idx += 1
+        return result
+
+    def from_string(self, s):
+        rv = []
+        for m in self._re_tokenize.finditer(s):
+            if m is None:
+                raise psycopg2.InterfaceError("bad pgtype representation: %r" % s)
+            if m.group(1) is not None:
+                rv.append(None)
+            elif m.group(2) is not None:
+                rv.append(self._re_undouble.sub(r"\1", m.group(2)))
+            else:
+                rv.append(m.group(3))
+
+        self.from_tuple(tuple(rv))
+
+    def adapt_tuple(self, t):
+        l = []
+        for i in t:
+            l.append(self._adapt(i))
+        return tuple(l)
+
+    @staticmethod
+    def repr_helper2(field_list):
+        result = '('
+        idx = 0
+        length = len(field_list) - 1
+        for item in field_list:
+            if idx < length:
+                result = result + "{1}{0}{2} ".format(idx, '{t[', ']},')
+            else:
+                result = result + "{1}{0}{2}::{3}.{4} ".format(idx, '{t[', ']})', '{schema}', '{pgtype}')
+            idx += 1
+        return result
+
+    def getquoted(self):
+        return self.repr_helper2(self.pg_field_list) \
+            .format(schema=self.pg_schm_name, pgtype=self.pg_type_name, t=self.adapt_tuple(self.to_tuple()))
 
 
-class FacilityHead(object):
+class FacilityHead(PgUserTypeMaping):
+    pg_schm_name = 'common'
+    pg_type_name = 'facility_head'
+    pg_field_list = ['document_id', 'gid', 'facility_code',
+                     'version_num', 'display_name', 'document_date',
+                     'parent_facility_code', 'facility_type']
+
     def __init__(self, s=None, curs=None):
         self.document_id = None
         self.gid = None
@@ -87,23 +183,7 @@ class FacilityHead(object):
         self.facility_type = None
         if s:
             self.from_string(s)
-
-    def __repr__(self):
-        return "facility_head(document_id={0}, \
-                gid={1}, facility_code={2}, version_num={3}, \
-                display_name={4}, document_date={5}, parent_facility_code={6}, facility_type={7})". \
-            format(self.document_id,
-                   self.gid,
-                   self.facility_code,
-                   self.version_num,
-                   self.display_name,
-                   self.document_date,
-                   self.parent_facility_code,
-                   self.facility_type)
-
-    def __conform__(self, proto):
-        if proto == _ext.ISQLQuote:
-            return self
+            print(self)
 
     def to_dict(self):
         if self.document_date:
@@ -147,54 +227,24 @@ class FacilityHead(object):
         self.parent_facility_code = t[6]
         self.facility_type = t[7]
 
-    def from_string(self, s):
-        if s is None:
-            return None
-        m = re.match(r"\((\"[^\"]*\"|[^,]+)?,(\"[^\"]*\"|[^,]+)?,(\"[^\"]*\"|[^,]+)?,(\"[^\"]*\"|[^,]+)?,(\"[^\"]*\"|[^,]+)?,(\"[^\"]*\"|[^,]+)?,(\"[^\"]*\"|[^,]+)?,(\"[^\"]*\"|[^,]+)?\)", s)
-        if m:
-            self.from_tuple((m.group(1),
-                             m.group(2),
-                             m.group(3),
-                             m.group(4),
-                             m.group(5),
-                             m.group(6),
-                             m.group(7),
-                             m.group(8)))
-        else:
-            raise psycopg2.InterfaceError("bad facility_head representation: %r" % s)
-
-    def getquoted(self):
-        return "({0}, {1}, {2}, {3}, {4}, {5}, {6}, {7})::common.facility_head" \
-            .format(_adapt(self.document_id),
-                    _adapt(self.gid),
-                    _adapt(self.facility_code),
-                    _adapt(self.version_num),
-                    _adapt(self.display_name),
-                    _adapt(self.document_date),
-                    _adapt(self.parent_facility_code),
-                    _adapt(self.facility_type))
+    def to_tuple(self):
+        return (self.document_id,
+                self.gid,
+                self.facility_code,
+                self.version_num,
+                self.display_name,
+                self.document_date,
+                self.parent_facility_code,
+                self.facility_type)
 
 
-def register_common_facility_head(oid=None, conn_or_curs=None):
-    if not oid:
-        oid1 = _get_pg_typname_oid(conn_or_curs, 'common', 'facility_head')
-        oid2 = _get_pg_typarray_oid(conn_or_curs, 'common', 'facility_head')
-    elif isinstance(oid, (list, tuple)):
-        oid1, oid2 = oid
-    else:
-        print('error')
-        exit(1)
+class InventoryHead(PgUserTypeMaping):
+    pg_schm_name = 'common'
+    pg_type_name = 'inventory_head'
+    pg_field_list = ['document_id', 'gid', 'display_name',
+                     'part_code', 'version_num', 'document_date',
+                     'uom_code', 'curr_fsmt', 'document_type']
 
-    FACILITY_HEAD = _ext.new_type((oid1,), "FACILITY_HEAD", FacilityHead)
-    FACILITY_HEAD_ARRAY = _ext.new_array_type((oid2,), "FACILITY_HEAD_ARRAY", FACILITY_HEAD)
-
-    _ext.register_type(FACILITY_HEAD, conn_or_curs)
-    _ext.register_type(FACILITY_HEAD_ARRAY, conn_or_curs)
-
-    return FACILITY_HEAD
-
-
-class InventoryHead(object):
     def __init__(self, s=None, curs=None):
         self.document_id = None
         self.gid = None
@@ -207,24 +257,7 @@ class InventoryHead(object):
         self.document_type = None
         if s:
             self.from_string(s)
-
-    def __repr__(self):
-        return "inventory_head(document_id={0}, \
-                gid={1}, display_name={2}, part_code={3}, \
-                version_num={4}, document_date={5}, uom_code={6}, curr_fsmt={7}, document_type={8})". \
-            format(self.document_id,
-                   self.gid,
-                   self.display_name,
-                   self.part_code,
-                   self.version_num,
-                   self.document_date,
-                   self.uom_code,
-                   self.curr_fsmt,
-                   self.document_type)
-
-    def __conform__(self, proto):
-        if proto == _ext.ISQLQuote:
-            return self
+            print(self)
 
     def to_dict(self):
         if self.document_date:
@@ -271,72 +304,30 @@ class InventoryHead(object):
         self.curr_fsmt = t[7]
         self.document_type = t[8]
 
-    def from_string(self, s):
-        if s is None:
-            return None
-        m = re.match(r"\((\"[^\"]*\"|[^,]+)?,(\"[^\"]*\"|[^,]+)?,(\"[^\"]*\"|[^,]+)?,(\"[^\"]*\"|[^,]+)?,(\"[^\"]*\"|[^,]+)?,(\"[^\"]*\"|[^,]+)?,(\"[^\"]*\"|[^,]+)?,(\"[^\"]*\"|[^,]+)?,(\"[^\"]*\"|[^,]+)?\)", s)
-        if m:
-            self.from_tuple((m.group(1),
-                             m.group(2),
-                             m.group(3),
-                             m.group(4),
-                             m.group(5),
-                             m.group(6),
-                             m.group(7),
-                             m.group(8),
-                             m.group(9)))
-        else:
-            raise psycopg2.InterfaceError("bad inventory_head representation: %r" % s)
-
-    def getquoted(self):
-        return "({0}, {1}, {2}, {3}, {4}, {5}, {6}, {7}, {8})::common.inventory_head" \
-            .format(_adapt(self.document_id),
-                    _adapt(self.gid),
-                    _adapt(self.display_name),
-                    _adapt(self.part_code),
-                    _adapt(self.version_num),
-                    _adapt(self.document_date),
-                    _adapt(self.uom_code),
-                    _adapt(self.curr_fsmt),
-                    _adapt(self.document_type))
+    def to_tuple(self):
+        return (self.document_id,
+                self.gid,
+                self.display_name,
+                self.part_code,
+                self.version_num,
+                self.document_date,
+                self.uom_code,
+                self.curr_fsmt,
+                self.document_type)
 
 
-def register_common_inventory_head(oid=None, conn_or_curs=None):
-    if not oid:
-        oid1 = _get_pg_typname_oid(conn_or_curs, 'common', 'inventory_head')
-        oid2 = _get_pg_typarray_oid(conn_or_curs, 'common', 'inventory_head')
-    elif isinstance(oid, (list, tuple)):
-        oid1, oid2 = oid
-    else:
-        print('error')
-        exit(1)
+class UnitConversion(PgUserTypeMaping):
+    pg_schm_name = 'common'
+    pg_type_name = 'unit_conversion_type'
+    pg_field_list = ['uom_code_from', 'uom_code_to', 'factor']
 
-    INVENTORY_HEAD = _ext.new_type((oid1,), "INVENTORY_HEAD", InventoryHead)
-    INVENTORY_HEAD_ARRAY = _ext.new_array_type((oid2,), "INVENTORY_HEAD_ARRAY", INVENTORY_HEAD)
-
-    _ext.register_type(INVENTORY_HEAD, conn_or_curs)
-    _ext.register_type(INVENTORY_HEAD_ARRAY, conn_or_curs)
-
-    return INVENTORY_HEAD
-
-class UnitConversion(object):
     def __init__(self, s=None, curs=None):
         self.uom_code_from = None
         self.uom_code_to = None
         self.factor = None
         if s:
             self.from_string(s)
-
-    def __repr__(self):
-        return "{0}(uom_code_from={1}, uom_code_to={2}, factor={3})" \
-            .format(type(self).__name__,
-                    self.uom_code_from,
-                    self.uom_code_to,
-                    str(self.factor))
-
-    def __conform__(self, proto):
-        if proto == _ext.ISQLQuote:
-            return self
+            print(self)
 
     def to_dict(self):
         return {"uom_code_from": self.uom_code_from,
@@ -353,45 +344,14 @@ class UnitConversion(object):
         self.uom_code_to = t[1]
         self.factor = Decimal(t[2])
 
-    def from_string(self, s):
-        if s is None:
-            return None
-        m = re.match(r"\((\"[^\"]*\"|[^,]+)?,(\"[^\"]*\"|[^,]+)?,(\"[^\"]*\"|[^,]+)?\)", s)
-        if m:
-            self.from_tuple((m.group(1),
-                             m.group(2),
-                             m.group(3)))
-        else:
-            raise psycopg2.InterfaceError("bad unit_conversion_type representation: %r" % s)
-
-    def getquoted(self):
-        return "({0}, {1}, {2})::common.unit_conversion_type"\
-            .format(_adapt(self.uom_code_from),
-                    _adapt(self.uom_code_to),
-                    _adapt(self.factor))
-
-
-def register_common_unit_conversion(oid=None, conn_or_curs=None):
-    if not oid:
-        oid1 = _get_pg_typname_oid(conn_or_curs, 'common', 'unit_conversion_type')
-        oid2 = _get_pg_typarray_oid(conn_or_curs, 'common', 'unit_conversion_type')
-    elif isinstance(oid, (list, tuple)):
-        oid1, oid2 = oid
-    else:
-        print('error')
-        exit(1)
-
-    UNIT_CONVERSION = _ext.new_type((oid1,), "UNIT_CONVERSION", UnitConversion)
-    UNIT_CONVERSION_ARRAY = _ext.new_array_type((oid2,), "UNIT_CONVERSION_ARRAY", UNIT_CONVERSION)
-
-    _ext.register_type(UNIT_CONVERSION, conn_or_curs)
-    _ext.register_type(UNIT_CONVERSION_ARRAY, conn_or_curs)
-
-    return UNIT_CONVERSION
+    def to_tuple(self):
+        return (self.uom_code_from,
+                self.uom_code_to,
+                self.factor)
 
 
 def register(conn):
     psycopg2.extras.register_uuid()
-    register_common_facility_head(conn_or_curs=conn)
-    register_common_inventory_head(conn_or_curs=conn)
-    register_common_unit_conversion(conn_or_curs=conn)
+    pg_typ_caster(connection=conn, nspname='common', typname='facility_head', mapclass=FacilityHead)
+    pg_typ_caster(connection=conn, nspname='common', typname='inventory_head', mapclass=InventoryHead)
+    pg_typ_caster(connection=conn, nspname='common', typname='unit_conversion_type', mapclass=UnitConversion)
